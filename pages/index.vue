b581f2f534acda147e27b7d16324f4891dd833c3
lukajaa <lucaskchang@gmail.com>
2023-11-10 13:58:53 -0800

    add scramble

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="pt-12">
-    <div class="mx-auto w-fit">
+    <div class="mx-auto w-1/2">
       <p class="text-center text-5xl font-bold">15 Puzzle</p>
       <p class="mb-2 text-center text-xl">
-        Use arrow keys to control the board. Press 'R' to reset.
+        Click on tiles or use arrow keys to control the board
       </p>
-      <div
-        class="flex flex-col space-y-2 rounded-lg bg-gray-950 p-4 dark:ring-2 dark:ring-white"
+      <TransitionGroup
+        name="grid-transition"
+        tag="div"
+        class="mx-auto flex w-full flex-wrap rounded-lg bg-gray-950 p-4 dark:ring-2 dark:ring-white"
       >
-        <div v-for="i in 4" :key="i" class="flex flex-row space-x-2">
-          <div v-for="j in 4" :key="j" class="flex flex-col">
-            <div
-              class="flex h-36 w-36 items-center justify-center rounded-lg"
-              :class="
-                grid[i - 1][j - 1] === 0
-                  ? 'bg-gray-950'
-                  : redSquares.includes(grid[i - 1][j - 1])
-                  ? 'bg-red-600'
-                  : 'bg-white'
-              "
+        <div
+          v-for="tile in displayGrid"
+          :key="tile"
+          class="flex w-1/4 flex-col p-2"
+        >
+          <div
+            class="flex h-36 w-36 items-center justify-center rounded-lg"
+            :class="
+              tile === 0
+                ? 'invisible'
+                : redSquares.includes(tile)
+                ? 'bg-red-600'
+                : 'bg-white'
+            "
+            @click="move(tile)"
+          >
+            <p
+              class="flex text-center font-serif text-7xl font-bold text-yellow-500"
             >
-              <p
-                class="flex text-center font-serif text-7xl font-bold text-yellow-500"
-              >
-                {{ grid[i - 1][j - 1] === 0 ? '' : grid[i - 1][j - 1] }}
-              </p>
-            </div>
+              {{ tile === 0 ? '' : tile }}
+            </p>
           </div>
         </div>
+      </TransitionGroup>
+      <div class="flex flex-row space-x-4">
+        <div class="flex w-1/2 flex-col">
+          <UButton class="mt-2" block @click="reset"> Reset (R) </UButton>
+        </div>
+        <div class="flex w-1/2 flex-col">
+          <UButton class="mt-2" block @click="scramble"> Scramble (S) </UButton>
+        </div>
       </div>
-      <UButton class="mt-2" block @click="reset"> Reset </UButton>
       <p
         class="mt-2 cursor-pointer text-center text-xl"
         @click="showMoves = !showMoves"
@@ -67,6 +79,16 @@ const grid = ref([
   [13, 14, 15, 0],
 ]);
 
+const displayGrid = computed(() => {
+  const tiles = [];
+  for (const row of grid.value) {
+    for (const tile of row) {
+      tiles.push(tile);
+    }
+  }
+  return tiles;
+});
+
 const redSquares = [2, 6, 10, 14, 4, 8, 12];
 const zeroLocation = [3, 3];
 const moves = ref(0);
@@ -84,6 +106,64 @@ function reset() {
   zeroLocation[1] = 3;
   moves.value = 0;
   movesList.value = [];
+}
+
+function scramble() {
+  reset();
+  for (let i = 0; i < 250; i++) {
+    const random = Math.floor(Math.random() * 4);
+    switch (random) {
+      case 0:
+        moveUp();
+        break;
+      case 1:
+        moveDown();
+        break;
+      case 2:
+        moveLeft();
+        break;
+      case 3:
+        moveRight();
+        break;
+    }
+  }
+  moves.value = 0;
+  movesList.value = [];
+}
+
+function move(tile: number) {
+  const [row, col] = zeroLocation;
+  const tileLocation = [0, 0];
+  for (let i = 0; i < grid.value.length; i++) {
+    for (let j = 0; j < grid.value[i].length; j++) {
+      if (grid.value[i][j] === tile) {
+        tileLocation[0] = i;
+        tileLocation[1] = j;
+        break;
+      }
+    }
+  }
+  if (row === tileLocation[0]) {
+    if (col < tileLocation[1]) {
+      for (let i = col; i < tileLocation[1]; i++) {
+        moveRight();
+      }
+    } else {
+      for (let i = col; i > tileLocation[1]; i--) {
+        moveLeft();
+      }
+    }
+  } else if (col === tileLocation[1]) {
+    if (row < tileLocation[0]) {
+      for (let i = row; i < tileLocation[0]; i++) {
+        moveDown();
+      }
+    } else {
+      for (let i = row; i > tileLocation[0]; i--) {
+        moveUp();
+      }
+    }
+  }
 }
 
 function moveUp() {
@@ -126,26 +206,35 @@ function moveRight() {
   movesList.value.unshift('right');
 }
 
-onKeyStroke(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'r'], (e) => {
-  e.preventDefault();
-  switch (e.key) {
-    case 'ArrowUp':
-      moveUp();
-      break;
-    case 'ArrowDown':
-      moveDown();
-      break;
-    case 'ArrowLeft':
-      moveLeft();
-      break;
-    case 'ArrowRight':
-      moveRight();
-      break;
-    case 'r':
-      reset();
-      break;
-  }
-});
+onKeyStroke(
+  ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'r', 's'],
+  (e) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        e.preventDefault();
+        moveUp();
+        break;
+      case 'ArrowDown':
+        e.preventDefault();
+        moveDown();
+        break;
+      case 'ArrowLeft':
+        e.preventDefault();
+        moveLeft();
+        break;
+      case 'ArrowRight':
+        e.preventDefault();
+        moveRight();
+        break;
+      case 'r':
+        reset();
+        break;
+      case 's':
+        scramble();
+        break;
+    }
+  },
+);
 
 // dark mode controls
 const colorMode = useColorMode();
@@ -158,3 +247,9 @@ const isDark = computed({
   },
 });
 </script>
+
+<style scoped>
+.grid-transition-move {
+  transition: transform 0.5s ease-in-out;
+}
+</style>
